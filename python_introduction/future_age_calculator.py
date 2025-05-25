f77af34ef4ddb9ea023b0acbb1e58782366c973e
systemphile <systemphile@gmail.com>
2025-05-25 14:09:00 +0300

    Python script to predict future age of user

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,6 @@
+#Python script to calculate future age of user
+current_age = int(input("Enter your current age: ")) #prompt user and convert age to integer
+future_age = current_age + 27 #calculate age in 2050
+
+#print future age
+print("In 2050, you will be ", future_age, " years old.")
