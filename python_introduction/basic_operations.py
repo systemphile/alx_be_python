10087e8181fb624b0fd8d213202f8b06335b4e59
systemphile <systemphile@gmail.com>
2025-05-25 13:16:39 +0300

    Python script that performs basic arithmetic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+#Python script that performs basic arithmetic operations with two predefined numbers
+
+number1 ,number2 = 10, 5 #Assign specific values
+sum = number1 + number2 #Find the sum of the numbers
+difference = number1 - number2 #Find the difference of the numbers
+product = number1 * number2 #Find the product of the numbers
+
+#Print results of each operation
+print(f"Addition of {number1} and {number2} is {sum} \nSubtraction of {number1} and {number2} is {difference} \nMultiplication of {number1} and {number2} is {product}")
