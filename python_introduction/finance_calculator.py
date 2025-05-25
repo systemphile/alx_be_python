eb834cf9a014240a52dea8516698bcf08e1505dc
systemphile <systemphile@gmail.com>
2025-05-25 14:41:58 +0300

    Variable name change

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,7 +1,7 @@
 #Python script to find user’s monthly savings and potential future savings
-income = float(input("Enter your monthly income: ")) #user provides monthly income
-expenses = float(input("Enter your total monthly expenses: ")) #user provides monthly expenses
-monthly_savings = income - expenses #caculate the monthly savings
+monthly_income = float(input("Enter your monthly income: ")) #user provides monthly income
+monthly_expenses = float(input("Enter your total monthly expenses: ")) #user provides monthly expenses
+monthly_savings = monthly_income - monthly_expenses #caculate the monthly savings
 projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05) #calculate the projected savings
 
 #print savings
