cbe9621174cdfa37efe0fbe5f8c89ac985c2de17
systemphile <systemphile@gmail.com>
2025-05-25 13:22:43 +0300

    Corrected variable assignment

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,6 +1,7 @@
 #Python script that performs basic arithmetic operations with two predefined numbers
 
-number1 ,number2 = 10, 5 #Assign specific values
+number1 = 10 #Assign specific value to number1
+number2 = 5 #Assign specific value to number2
 sum = number1 + number2 #Find the sum of the numbers
 difference = number1 - number2 #Find the difference of the numbers
 product = number1 * number2 #Find the product of the numbers
