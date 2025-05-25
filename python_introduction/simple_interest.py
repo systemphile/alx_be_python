4bcf39196350151a01873bd68169593ad2ff2e26
systemphile <systemphile@gmail.com>
2025-05-25 13:38:07 +0300

    Python script calculating simple interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,9 @@
+#Python script to calculate simple interest
+
+principal = 1000 #Assign value to principal variable
+rate = 0.05 #Assign value to rate variable
+time = 3 #Assign value to time variable
+interest = principal * rate * time #Calculate the simple interest
+
+#Print final result
+print("The simple interest is: ", interest)
