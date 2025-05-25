f1ee1aab20d87fb994d7546b44d661ca372628d9
systemphile <systemphile@gmail.com>
2025-05-25 14:38:34 +0300

    Corrected user input prompt and calculation of monthly savings

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,8 +1,8 @@
 #Python script to find user’s monthly savings and potential future savings
-income = float(input("Enter your monthly income: \n")) #user provides monthly income
-expenses = float(input("Enter your total monthly expenses: \n")) #user provides monthly expenses
-savings = income - expenses #caculate the monthly savings
-projected_savings = savings * 12 + (savings * 12 * 0.05) #calculate the projected savings
+income = float(input("Enter your monthly income: ")) #user provides monthly income
+expenses = float(input("Enter your total monthly expenses: ")) #user provides monthly expenses
+monthly_savings = income - expenses #caculate the monthly savings
+projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05) #calculate the projected savings
 
 #print savings
-print (f"Your monthly savings are {savings}\nProjected savings after one year, with interest, is: {projected_savings}")
+print (f"Your monthly savings are {monthly_savings}\nProjected savings after one year, with interest, is: {projected_savings}")
