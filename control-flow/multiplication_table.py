5a5b99a9756d96480a7e72b84f7e603d13d90567
systemphile <systemphile@gmail.com>
2025-05-30 19:34:25 +0300

    changed loop iteration variable

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -3,10 +3,7 @@
 #ask user for a number
 number = int(input("Enter a number to see its multiplication table:"))
 
-#define looping sequence
-factors = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
 #find product of number and multiplication factor
-for factor in factors:
-    product = number * factor
-    print(f"{number} * {factor} = {product}")
+for i in range(1,11):
+    product = number * i
+    print(f"{number} * {i} = {product}")
