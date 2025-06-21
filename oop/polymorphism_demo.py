9a3c0e7f349a6420ecbbe3077e3a8fbbfe36c1a2
systemphile <systemphile@gmail.com>
2025-06-21 12:51:05 +0300

    Exploring Polymorphism and Method Overriding

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -1,16 +1,20 @@
+# This script will inherit methods from parent class and implement polymorphism
 import math
 
+# define base class
 class Shape:
     def __init__(self):
         pass
     def area(self):
         raise NotImplementedError
+
+# define classes derived from base class
 class Rectangle(Shape):
     def __init__(self, length, width):
         super().__init__()
         self.length = length
         self.width = width
-    def area(self):
+    def area(self): # child class implementing its own behaviour
         return self.length * self.width
 
 class Circle(Shape):
